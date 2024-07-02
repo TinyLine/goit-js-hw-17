76cc6de87a77a20419e1080a22b81ed48e89cefd
Якименко Дмитро <108049675+TinyLine@users.noreply.github.com>
2024-07-02 19:11:31 +0300

    ms

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const apiKey = '44745318-d405a2bfe88f16496c90839a8';
 let page = 1;
 
 async function apiImageLink(apiKey, query, page) {
-        const response = await fetch(`https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(query)}&image_type=photo&page=${page}&per_page=6`);
+        const response = await fetch(`https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(query)}&image_type=photo&page=${page}&per_page=4`);
         const data = await response.json();
         return data.hits.map(hit => hit.webformatURL);
     }
